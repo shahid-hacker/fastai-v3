76ddcc63e30b7f1030ae53129224a5668d849839
shahid-hacker <65731653+shahid-hacker@users.noreply.github.com>
2020-07-28 09:30:29 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -64,7 +64,10 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    breed = str(prediction)
+    breed.replace("_"," ")
+    breed.capitalize()
+    return JSONResponse({'result': breed})
 
 
 if __name__ == '__main__':
