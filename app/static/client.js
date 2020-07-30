cb050229d80f984c783a5a24013ead3d4355c52c
shahid-hacker <65731653+shahid-hacker@users.noreply.github.com>
2020-07-30 15:49:26 -0400

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -18,7 +18,7 @@ function analyze() {
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-  el("analyze-button").innerHTML = "Analyzing...";
+  el("analyze-button").innerHTML = "Your pet is a...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
@@ -29,9 +29,9 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["Your pet is a"]}`;
+      el("result-label").innerHTML = `Your `,animal, `is a = ${response["Your ", animal, "is a"]}`;
     }
-    el("analyze-button").innerHTML = "Analyze";
+    el("analyze-button").innerHTML = "Predict breed";
   };
 
   var fileData = new FormData();
